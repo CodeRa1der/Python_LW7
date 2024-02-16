654d2ff9c0c070491357add6ada9b6f676cf12ef
Code_Ra1der <kostydu2342@gmail.com>
2024-02-16 13:03:27 +0300

    Made Homework

---
+++ Individual/individ1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+elements = list(map(int, input('Введите ряд из чисел, через пробел: ').split()))
+indexer = int(input('Введите число, ищющее индексы элементы списка: '))
+
+if indexer in elements:
+    index = elements.index(indexer)+1
+    print('Индекс заданного элемента:', index)
+else:
+    print('Заданного элемента нет в списке')
---
+++ Individual/individ2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+list = list(map(int, input('Введите целые элементы через пробел: ').split()))
+even = []
+odd = []
+count = 0
+sum = 0
+min = 0
+minnum = 0
+
+for i in range(len(list)):
+    k = list[i]
+    # Запись четных и нечетных элементов
+    if (list.index(k) + 1) % 2 == 0:
+        even.append(list[i])
+    else:
+        odd.append(list[i])
+    # Задача №1
+    if list.index(k) == 0:
+        min = abs(list[i])
+        minnum = list[i]
+    if abs(list[i]) < min:
+        min = abs(list[i])
+        minnum = list[i]
+     # Задача №2
+    if list[i] == 0:
+        count += 1
+    if count != 0:
+        sum += list[i]
+
+print('')
+print('Минимальный по модулю элемент списка:', minnum)
+if sum != 0:
+    print('Сумма элементов после первого нуля:', sum)
+else:
+    print('В списке нет элементов, равных нулю')
+print('Реорганизованный список:')
+print(even + odd)
