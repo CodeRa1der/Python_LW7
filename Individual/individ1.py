b79a7a9969417feb29c184ea9c543841d511f015
Code_Ra1der <kostydu2342@gmail.com>
2024-02-16 20:25:06 +0300

    Minor fixes

--- Individual/individ1.py
+++ Individual/individ1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-elements = list(map(int, input('Введите ряд из чисел, через пробел: ').split()))
-indexer = int(input('Введите число, ищющее индексы элементы списка: '))
+if __name__ == '__main__':
+    elements = list(map(int, input('Введите ряд из чисел, через пробел: ').split()))
+    indexer = int(input('Введите число, ищющее индексы элементы списка: '))
 
-if indexer in elements:
-    index = elements.index(indexer)+1
-    print('Индекс заданного элемента:', index)
-else:
-    print('Заданного элемента нет в списке')
+    if indexer in elements:
+        index = elements.index(indexer)+1
+        print('Индекс заданного элемента:', index)
+    else:
+        print('Заданного элемента нет в списке')
